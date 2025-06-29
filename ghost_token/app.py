53256211d66d13bb9a760f03da0a4ee30c5970a9
Carlos Jimenez Diaz, FRM <88047731+carlosjimenezdiaz@users.noreply.github.com>
2025-06-29 09:39:08 -0400

    Update app.py

--- ghost_token/app.py
+++ ghost_token/app.py
@@ -2,18 +2,22 @@ import jwt
 import time
 from flask import Flask, jsonify
 import os
+from dotenv import load_dotenv
+
+# Load env vars from .env file
+load_dotenv()
 
 app = Flask(__name__)
 
 @app.route("/ghost-token", methods=["GET"])
 def get_token():
-    key = os.environ.get("GHOST_ADMIN_API_KEY")  # formato: id:secret
+    key = os.environ.get("GHOST_ADMIN_API_KEY")
     if not key or ":" not in key:
         return jsonify({"error": "Invalid Admin API Key"}), 400
 
     key_id, secret = key.split(":")
     iat = int(time.time())
-    exp = iat + 5 * 60  # 5 minutos de validez
+    exp = iat + 5 * 60
 
     header = {
         "alg": "HS256",
@@ -29,6 +33,3 @@ def get_token():
 
     token = jwt.encode(payload, bytes.fromhex(secret), algorithm="HS256", headers=header)
     return jsonify({"token": token})
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
